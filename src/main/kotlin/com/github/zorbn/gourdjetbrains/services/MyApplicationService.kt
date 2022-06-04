6bbd91fbe9cbb3ec2377ec3959103554a3df1d4a
GitHub Action <action@github.com>
2022-06-04 17:56:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/zorbn/gourdjetbrains/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zorbn/gourdjetbrains/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zorbn.gourdjetbrains.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zorbn.gourdjetbrains.MyBundle
 
 class MyApplicationService {
 
